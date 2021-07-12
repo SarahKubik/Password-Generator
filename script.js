022a2e1c51c12ec0d901af3e2e0cd952039f16f3
Sarah <81164320+SarahKubik@users.noreply.github.com>
2021-07-11 23:50:27 -0400

    update script

--- script.js
+++ script.js
@@ -123,3 +123,17 @@ function getPasswordOptions() {
   var hasNumericCharacters = confirm(
     'Click OK to confirm including numeric characters.'
   )}
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
